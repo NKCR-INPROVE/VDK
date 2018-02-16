e2848df3ce6db557e22a63dad229db9854d7ea85
alberto.a.hernandez <alberto.a.hernandez@CPX-S3MHW0IN0DT.dir.svc.accenture.com>
2018-02-16 10:46:40 +0100

    dynamicke zmeny

--- vdk/src/main/ng-client/src/app/app.component.ts
+++ vdk/src/main/ng-client/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  view: boolean;
+  rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  text: string = "Vestibulum erat nullaVestibulum erat nulla";
+  toggle(){
+	  this.view = !this.view;
+	  this.text += this.text;
+  }
 }
